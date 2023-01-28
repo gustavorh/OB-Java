7371e7ab80a2ff76b9e07c9caad09eb9b2386462
Gustavo <gustavocars@gmail.com>
2023-01-27 23:10:23 -0300

    Init commit.

---
+++ src/com/openbootcamp/java/Main.java
@@ -0,0 +1,17 @@
+package com.openbootcamp.java;
+
+public class Main {
+    public static void main(String[] args) {
+        int valorInt = 0;
+        long valorLong = 23947893;
+        double valorDouble = 1.0;
+        boolean valorBoolean = true;
+        String valorString = "Este es un String";
+
+        System.out.println(valorInt);
+        System.out.println(valorLong);
+        System.out.println(valorDouble);
+        System.out.println(valorBoolean);
+        System.out.println(valorString);
+    }
+}
